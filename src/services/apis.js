62f1d9e1e6cf1cd67717f23772ca0f972befe713
Bhawani Shankar Sarswat <130194346+bssCoder@users.noreply.github.com>
2024-06-26 00:02:14 +0530

    Update apis.js

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,6 +1,6 @@
 
 // require("dotenv").config();
-const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = "https://techtutor.onrender.com/api/v1"
 // AUTH ENDPOINTS
 export const endpoints = {
   SENDOTP_API: BASE_URL + "/auth/sendotp",
